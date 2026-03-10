31b67668084b69218e9bc2c3a527bb1aa8884c34
BIANCA LO <30097639@tafe.wa.edu.au>
2026-03-10 22:48:04 +0800

    Add project files.

---
+++ CityLink-SCP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CityLink_SCP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
