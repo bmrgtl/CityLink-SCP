729b6f375abbcc8b6859187a86af467bd43b50d8
30097639 <30097639@MD-T108-603932>
2026-04-22 11:44:22 +0800

    - finished the final booking form - added book service link to homecontroller - linked service buttons to book a service

--- CityLink-SCP/Controllers/HomeController.cs
+++ CityLink-SCP/Controllers/HomeController.cs
@@ -48,6 +48,10 @@ namespace CityLink_SCP.Controllers
         {
             return View();
         }
+        public IActionResult BookService()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
