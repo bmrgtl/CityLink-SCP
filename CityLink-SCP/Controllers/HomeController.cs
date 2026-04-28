f0c4cdab337aac7020835ef2ffe1bed299a56492
BIANCA LO <30097639@tafe.wa.edu.au>
2026-04-28 22:06:10 +0800

    - made news page - made book services form responsive

--- CityLink-SCP/Controllers/HomeController.cs
+++ CityLink-SCP/Controllers/HomeController.cs
@@ -47,6 +47,11 @@ namespace CityLink_SCP.Controllers
             return View();
         }
 
+		public IActionResult News()
+		{
+			return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
